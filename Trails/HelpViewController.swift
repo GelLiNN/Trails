db573232c81c95428ef5a500c0a853af8717eb00
knealy <knealy@splunk.com>
2016-01-29 21:20:06 -0800

    added initial project files

---
+++ Trails/HelpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HelpViewController.swift
+//  Trails
+//
+//  Created by Kellan Nealy on 1/29/16.
+//  Copyright © 2016 Expedia. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
